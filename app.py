7dd9e2cbd3202512fdd8f8647086ffc83236018a
taewan <ntw2100@gmail.com>
2022-04-21 11:45:53 +0900

    데이터 호출 코드 작성
    서버 -> 화면으로 상품 데이터 보내는 코드작성

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route('/allList', methods=['GET'])
+def list_all():
+    all_list = list(db.tour.find({},{'_id':False}))
+    print(all_list)
+    # print('avc')
+    return jsonify({'all_list':all_list})
+    # return jsonify({'all_list':'avc'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
