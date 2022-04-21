c9f6b1799ef2b260c43871ad19d04bb72abd8048
taewan <ntw2100@gmail.com>
2022-04-21 14:50:22 +0900

    공백, 주석 제거

--- app.py
+++ app.py
@@ -15,10 +15,8 @@ def home():
 @app.route('/allList', methods=['GET'])
 def list_all():
     all_list = list(db.tour.find({},{'_id':False}))
-    print(all_list)
-    # print('avc')
+
     return jsonify({'all_list':all_list})
-    # return jsonify({'all_list':'avc'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
